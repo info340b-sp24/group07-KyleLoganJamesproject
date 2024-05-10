49467155ff5f39e6958b6f1223907284111bf690
KyleDuCS <Kyled23@uw.edu>
2024-05-10 00:25:29 -0700

    currently broken cant start

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {App} from './components/App';
-import carList from './data/cars.json';
 import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
+import './style.css';
+import { App } from './components/App';
+import carList from './data/cars.json';
 
 
 // ADD JSON OBJECT IMPORT AND PASS INTO APP as a prop
