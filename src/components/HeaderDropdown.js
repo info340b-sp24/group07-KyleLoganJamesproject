de53ef1971ec60e2a5b229886cbeffd6e1512162
Logan Dinh <logandinh@Logans-MacBook-Pro-2.local>
2024-05-15 18:00:25 -0700

    cleaner logan files

--- src/components/HeaderDropdown.js
+++ src/components/HeaderDropdown.js
@@ -3,7 +3,7 @@ import React from 'react';
 export function HeaderDropdown(props) {
 
     // update he href after we learn how to route pages
-    console.log(props.props)
+    // console.log(props.props)
 
     const links = props.props.map((link, index) => {
         return <a key={index} className="nav-link active text-dark fs-2" aria-current={link} href={link}>{link}</a>
