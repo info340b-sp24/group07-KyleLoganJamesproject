fbd749d1be61d5a2b1258beb191ef36be0b50552
KyleDuCS <Kyled23@uw.edu>
2024-05-17 10:56:28 -0700

    removed comments

--- src/components/HeaderDropdown.js
+++ src/components/HeaderDropdown.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
 export function HeaderDropdown(props) {
-
-    // update he href after we learn how to route pages
-    // console.log(props.props)
-
     const links = props.props.map((link, index) => {
         return <a key={index} className="nav-link active text-dark fs-2" aria-current={link} href={link}>{link}</a>
     });
