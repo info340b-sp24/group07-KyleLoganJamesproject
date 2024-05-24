9d7f8c169194d84024dda018ac7edefc55a6e609
KyleDuCS <Kyled23@uw.edu>
2024-05-23 18:16:17 -0700

    added navlinks

--- src/components/HeaderNonActiveDropdown.js
+++ src/components/HeaderNonActiveDropdown.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export function HeaderNonActiveDropdown(props) {
     const links = props.props.map((link, index) => {
         if(index === 0){
-            return <a key={index} className="nav-link active text-white fs-2 mt-3 p-0" aria-current={link} href={link}>{link}</a>
+            return <li><NavLink to={"/"+link} className="nav-link active text-white fs-2 mt-3 p-0" aria-current={link}>{link}</NavLink></li>
         } else {
-            return <a key={index} className="nav-link active text-white fs-2 p-0" aria-current={link} href={link}>{link}</a>
+            return <li><NavLink to={"/"+link} className="nav-link active text-white fs-2 p-0" aria-current={link}>{link}</NavLink></li>
         }
+       
     });
 
     return (
