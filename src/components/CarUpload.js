d182c571d53eb6dfc9fd255823cb0a359282992f
jamesdwa <jdwa@uw.edu>
2024-05-26 13:10:34 -0700

    completed a functional Firebase

--- src/components/CarUpload.js
+++ src/components/CarUpload.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { CarPicture } from './CarPicture';
 import { CarUploadText } from './CarUploadText';
 
-
+// CarUpload acts as the parent-component to child-components CarPicture and CarUploadText
+// Easier to format together than individually
 export function CarUpload (props) {
   return (
       <div className="flex-container">
--- src/components/CarUploadText.js
+++ src/components/CarUploadText.js
@@ -1,139 +1,166 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { CarUploadDirections } from './CarUploadDirections';
-import { getDatabase, ref, onValue, push as FirebasePush} from 'firebase/database'; 
+import { getDatabase, ref, onValue, push as FirebasePush } from 'firebase/database';
 
-// this is where you will define all of the forms for uploading to the car,
-// any extra components for the options should be rendered on this page
-export function CarUploadText(){
+export function CarUploadText() {
     const carTypes = ["Car Category", "Truck", "Sedan", "Sports", "Luxury", "SUV"];
 
-    const categoryOptions = carTypes.map((carType) => {
-        return (
-            <option value={carType}>{carType}</option>
-        )
-    });
+    const categoryOptions = carTypes.map((carType, index) => (
+        <option key={index} value={carType}>{carType}</option>
+    ));
 
-    const luxuryOptions = [<option value={"insert"}>{"Luxury Scale"}</option>];
-    for(let i = 1; i <= 10; i++){
-        luxuryOptions.push(<option value={i}>{i}</option>);
+    const luxuryOptions = [<option key={0} value={"insert"}>{"Luxury Scale"}</option>];
+    for (let i = 1; i <= 10; i++) {
+        luxuryOptions.push(<option key={i} value={i}>{i}</option>);
     }
 
-    const safetyScale = [<option value={"insert"}>{"Safety Scale"}</option>];
-    for(let i = 1; i <= 5; i++){
-        safetyScale.push(<option value={i}>{i}</option>);
+    const safetyScale = [<option key={0} value={"insert"}>{"Safety Scale"}</option>];
+    for (let i = 1; i <= 5; i++) {
+        safetyScale.push(<option key={i} value={i}>{i}</option>);
     }
 
-    // const [state, setState] = useState("");
+    const [carState, setCarState] = useState("");
+    const [categoryState, setCategoryState] = useState("");
+    const [mghState, setMghState] = useState("");
+    const [priceState, setPriceState] = useState("");
+    const [luxuryState, setLuxuryState] = useState("");
+    const [safetyState, setSafetyState] = useState("");
+    const [webState, setWebState] = useState("");
+    const [descState, setDescState] = useState("");
 
-    // const db = getDatabase();
-    // const allMessagesRef = ref(db, "allMessages");
     
-    // useEffect(() => {
-        
-    //     const offFunction = onValue(allMessagesRef, function(snapshot) {
-    //     const allMessagesObj = snapshot.val();
-    //     console.log(allMessagesObj);
-    //     const objKeys = Object.keys(allMessagesObj);
-        
-    //     const objArray = objKeys.map((keyString) => {
-    //         // allMessagesObj[keyString].key = keyString;
-    //         console.log(keyString)
-    //         return allMessagesObj[keyString];
-    //     })
-        
-    //     // setChatMessages(objArray); //update state and re-render
-        
-    //     function cleanup() {
-    //         console.log("Component is being removed")
-    //         offFunction();
-    //     }
-    //         return cleanup;
-    //     })
-    // }, []);
+    const handleCarNameChange = (event) => {
+        const newValue = event.target.value
+        setCarState(newValue);
+    }
+    
+    const handleCategoryChange = (event) => {
+        const newValue = event.target.value
+        setCategoryState(newValue);
+    }
 
+    const handleMghChange = (event) => {
+        const newValue = event.target.value
+        setMghState(newValue);
+    } 
 
-    // const addMessage = (messageText) => {
-    //     const newMessageObj = {
-    //     "car_name": null,
-    //     "description": null,
-    //     "type": null,
-    //     "link": null,
-    //     "image": null,
-    //     "price": null,
-    //     "luxury_scale": null,
-    //     "saftey_rating": null,
-    //     "MPG": null
-    //     } 
+    const handlePriceChange = (event) => {
+        const newValue = event.target.value
+        setPriceState(newValue);
+    }
+    
+    const handleLuxuryChange = (event) => {
+        const newValue = event.target.value
+        setLuxuryState(newValue);
+    }
+    const handleSafetyChange = (event) => {
+        const newValue = event.target.value
+        setSafetyState(newValue);
+    }
 
-    //     const updateChatMessages = [...chatMessages, newMessage];
-    //     setChatMessages(updateChatMessages); //update state and re-render
-    //     const db = getDatabase();
-    //     const allMessagesRef = ref(db,"allMessages");
+    const handleWebChange = (event) => {
+        const newValue = event.target.value
+        setWebState(newValue);
+    }
+    const handleDescChange = (event) => {
+        const newValue = event.target.value
+        setDescState(newValue);
+    }
+    
+    const carDataUrl = "/";
 
-    //     // firebaseSet(messageRef, newMessageObj);
-    //     FirebasePush(allMessagesRef, newMessageObj);
-    // }
+    const db = getDatabase();
+    const carDataRef = ref(db, carDataUrl);
+    
+    useEffect(() => {
+
+        const offFunction = onValue(carDataRef, function(snapshot) {
+            const allCarDataObj = snapshot.val();
+            console.log(allCarDataObj);
+        });
+
+        function cleanup() {
+            console.log("Component is being removed")
+            offFunction();
+        }
+        return cleanup;
+    }, [carDataRef]); // useEffect will run again once the value of carDataRef changes
+
+    const addCar = (event) => {
+        // Overrides the browser sending the form data to the current URL and refreshing the page
+        event.preventDefault();
+
+        const newUploadObj = {
+            "car_name": carState,
+            "description": descState,
+            "type": categoryState,
+            "link": webState,
+            "image": null,
+            "price": priceState,
+            "luxury_scale": luxuryState,
+            "safety_rating": safetyState,
+            "MPG": mghState
+        };
+
+        FirebasePush(carDataRef, newUploadObj);
+    };
 
     return (
         <div className="right-section text-center">
             <CarUploadDirections />
             <div className="row">
                 <div className="col-md-6">
-                    <h3 className="header-font"> Car Name</h3>
-                    <input type="text" className="form-control mb-3" placeholder="Car Name" />
+                    <h3 className="header-font">Car Name</h3>
+                    <input type="text" className="form-control mb-3" placeholder="Car Name" value={carState} onChange={handleCarNameChange} />
                 </div>
                 <div className="col-md-6">
-                    <form action="#">
                     <div className="form-group">
                         <label className="header-font" htmlFor="carCategory">Car Category</label>
-                        <select className="form-control mb-3" id="carCategory" name="carCategory">
+                        <select className="form-control mb-3" id="carCategory" name="carCategory" value={categoryState} onChange={handleCategoryChange}>
                             {categoryOptions}
                         </select>
                     </div>
-                    </form>
                 </div>
                 <div className="col-md-6">
-                    <h3 className="header-font">Mile Per Gallon (MGH)</h3>
-                    <input type="text" className="form-control mb-3" placeholder="MGH" />
+                    <h3 className="header-font">Mile Per Gallon (MPG)</h3>
+                    <input type="text" className="form-control mb-3" placeholder="MPG" value={mghState} onChange={handleMghChange} />
                 </div>
                 <div className="col-md-6">
                     <h3 className="header-font">Price</h3>
-                    <input type="text" className="form-control mb-3" placeholder="Price" />
+                    <input type="text" className="form-control mb-3" placeholder="Price" value={priceState} onChange={handlePriceChange} />
                 </div>
                 <div className="col-md-6">
-                    <form action="#">
                     <div className="form-group">
                         <label className="header-font" htmlFor="luxuryScale">Luxury Scale (1-10)</label>
-                        <select className="form-control mb-3" id="luxuryScale" name="luxuryScale">
-                        {luxuryOptions}
+                        <select className="form-control mb-3" id="luxuryScale" name="luxuryScale" value={luxuryState} onChange={handleLuxuryChange}>
+                            {luxuryOptions}
                         </select>
                     </div>
-                    </form>
                 </div>
                 <div className="col-md-6">
-                    <form action="#">
                     <div className="form-group">
                         <label className="header-font" htmlFor="safetyScale">Safety Scale (1-5)</label>
-                        <select className="form-control mb-3" id="safetyScale" name="safetyScale">
+                        <select className="form-control mb-3" id="safetyScale" name="safetyScale" value={safetyState} onChange={handleSafetyChange}>
                             {safetyScale}
                         </select>
                     </div>
-                    </form>
                 </div>
                 <div className="col-md-12">
                     <h3 className="header-font">Webpage Link</h3>
-                    <input type="text" className="form-control mb-3" placeholder="Paste the Webpage Link" />
+                    <input type="text" className="form-control mb-3" placeholder="Paste the Webpage Link" value={webState} onChange={handleWebChange} />
                 </div>
                 <div className="col-md-12">
                     <h3 className="header-font">Description</h3>
-                    <textarea className="form-control mb-3" placeholder="Enter the Car's Description" />
+                    <textarea className="form-control mb-3" placeholder="Enter the Car's Description" value={descState} onChange={handleDescChange} />
                 </div>
             </div>
             <div>
-                <button className="btn btn-primary" type="upload">
-                    <a className="text-white text-decoration-none" href="App.js">Upload</a>
-                </button>
+                <form onSubmit={addCar}>
+                    <button className="btn btn-primary" type="upload">
+                        <a className="text-white text-decoration-none" href="App.js">Upload</a>
+                   </button>
+                </form>
             </div>
         </div>
-    )
+    );
 }
