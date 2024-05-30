5201657ad9860b1fbb05906a95ab4c6173e5177e
Logan Dinh <logandinh@Logans-MacBook-Pro-2.local>
2024-05-30 15:03:25 -0700

    home page changes

--- src/components/HeaderNonActiveDropdown.js
+++ src/components/HeaderNonActiveDropdown.js
@@ -4,9 +4,9 @@ import { NavLink } from 'react-router-dom';
 export function HeaderNonActiveDropdown(props) {
     const links = props.props.map((link, index) => {
         if(index === 0){
-            return <li><NavLink to={"/"+link} className="nav-link text-white fs-2 mt-3 p-0" aria-current={link}>{link}</NavLink></li>
+            return <li key={index}><NavLink to={"/"+link} className="nav-link text-white fs-2 mt-3 p-0" aria-current={link}>{link}</NavLink></li>
         } else {
-            return <li><NavLink to={"/"+link} className="nav-link text-white fs-2 p-0" aria-current={link}>{link}</NavLink></li>
+            return <li key={index}><NavLink to={"/"+link} className="nav-link text-white fs-2 p-0" aria-current={link}>{link}</NavLink></li>
         }
        
     });
